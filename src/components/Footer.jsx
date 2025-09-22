b9dbf4214c6d2cc15dcdab52da90b4fa55950496
CHETHAN B R <chethanbrp98@gmail.com>
2025-09-22 17:41:05 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
           <div className=" flex flex-col items-center justify-center">
             <div className="flex space-x-4">
-              <FaGitHub size={24} />
+              < size={24} />
               <FaLinkedinIn size={24} />
             </div>
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
