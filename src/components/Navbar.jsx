279037135f27c37d400d1a36f1d67429a3012d56
CHETHAN B R <chethanbrp98@gmail.com>
2025-09-22 16:58:15 +0530

    Update Navbar.jsx
    navbar change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,8 +34,8 @@ function Navbar() {
           <div className=" flex space-x-2">
             <img src={pic} className="h-12 w-12 rounded-full" alt="" />
             <h1 className="font-semibold text-xl cursor-pointer">
-              Akhi<span className="text-green-500 text-2xl">l</span>
-              <p className="text-sm">Web Developer</p>
+              Chethan<span className="text-green-500 text-2xl">l</span>
+              <p className="text-sm">AWS DevOps Engineer</p>
             </h1>
           </div>
           {/* desktop navbar */}
